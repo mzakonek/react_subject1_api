eb92eceae0f33011ff156d44d61045694fbd4581
mzakonek <unnamed200@gmail.com>
2020-07-02 17:03:22 +0200

    update variables

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -11,10 +11,10 @@ export default class UserList extends React.Component {
     state = {
         users: [],
         ready: false,
-        userPerPage: 10
+        usersPerPage: 10
     };
     componentDidMount() {
-        fetch("https://randomuser.me/api/?results=10")
+        fetch(`https://randomuser.me/api/?results=${this.state.usersPerPage}`)
         .then(response => {return response.json()})
         .then(data => {
             setTimeout(() => this.setState({ ready: true, users: data.results}), 4000);
@@ -25,7 +25,7 @@ export default class UserList extends React.Component {
     render() {
         return(
             <div>
-                <ReactPlaceholder type='text' rows={this.state.userPerPage} ready={this.state.ready}>
+                <ReactPlaceholder type='text' rows={this.state.usersPerPage} ready={this.state.ready}>
                     {this.state.users.map(user => (
                         <div key={user.login.uuid}>
                             <Link to={{pathname: `/user/${user.login.uuid}`, userData: user}}>
